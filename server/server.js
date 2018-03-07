83bcc5d551ac59546403cf894bdcda0b4b627d77
Aaron Ponce <aaron.m.ponce@gmail.com>
2018-03-06 22:00:28 -0500

    add mentions and recents to server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-
 module.exports = function() {
   var faker = require("faker");
   var _ = require("lodash");
   return {
-    Twitter: _.times(20, function(n) {
+    Data: _.times(20, function(n) {
       return {
+     Twitter:{
         ChannelId: n,
         TwitterAccount: faker.random.alphaNumeric(),
         Id: faker.random.number(),
@@ -13,6 +13,27 @@ module.exports = function() {
         ThumbnailUrl: faker.image.avatar(),
         Date: faker.date.recent(),
         Text: faker.lorem.sentence()
+     },
+     Mentions: {
+        ChannelId: n,
+        TwitterAccount: faker.random.alphaNumeric(),
+        Id: faker.random.number(),
+        Handle: faker.internet.userName(),
+        Name: faker.name.findName(),
+        ThumbnailUrl: faker.image.avatar(),
+        Date: faker.date.recent(),
+        Text: faker.lorem.sentence()
+     },
+      Recents: {
+        ChannelId: n,
+        TwitterAccount: faker.random.alphaNumeric(),
+        Id: faker.random.number(),
+        Handle: faker.internet.userName(),
+        Name: faker.name.findName(),
+        ThumbnailUrl: faker.image.avatar(),
+        Date: faker.date.recent(),
+        Text: faker.lorem.sentence()
+        }
       };
     })
   };
